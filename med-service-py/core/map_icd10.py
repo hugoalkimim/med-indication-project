3511a08828bf7dced4a846da4f8cf5287a283cdb
Hugo Alkimim <hugodanilosantosalkimim@gmail.com>
2025-03-24 06:49:11 -0300

    chore(med-service-py):  clean up unused code

--- med-service-py/core/map_icd10.py
+++ med-service-py/core/map_icd10.py
@@ -66,13 +66,3 @@ def map_conditions_to_icd10(
             })
 
     return mappings
-
-if __name__ == "__main__":
-    print("Loading ICD-10 codes...")
-    icd10_codes = load_icd10_from_txt("../data/icd10cm-codes-April-2025.txt")
-    print(f"Loaded {len(icd10_codes)} ICD-10 codes")
-    conditions = ["asthma", "diabetes", "hypertension", "hyperlipidemia", "anemia"]
-    mappings = map_conditions_to_icd10(conditions, icd10_codes,  similarity_threshold=0.5)
-    print(mappings)
-    for mapping in mappings:
-        print(f"{mapping['condition']} -> {mapping['icd10']}")

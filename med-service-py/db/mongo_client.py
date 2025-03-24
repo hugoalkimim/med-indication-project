79a670c343914a1c43c478a052ecb94ca0c8460b
Hugo Alkimim <hugodanilosantosalkimim@gmail.com>
2025-03-24 00:52:53 -0300

    fix(med-service-py): rename drug_mappings collection

--- med-service-py/db/mongo_client.py
+++ med-service-py/db/mongo_client.py
@@ -8,11 +8,11 @@ MONGO_DB = os.getenv("MONGO_DB", "med-indications")
 client = MongoClient(MONGO_URI)
 db = client[MONGO_DB]
 
-drug_mappings = db["drug_mappings"]
+med_mappings = db["mappings"]
 
 def insert_mapping(document: dict):
     """
     Inserts a new drug mapping document into MongoDB.
     """
-    drug_mappings.insert_one(document)
+    med_mappings.insert_one(document)
     print(f"✅ Inserted mapping for: {document.get('drug')}")
